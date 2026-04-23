206794a593bb0797b79521a041a3b9c1268b796b
Resky <1411011619@qq.com>
2026-04-23 15:16:26 +0800

    26.4.23 Vue项目生成功能
    1.定制LangChain4j源码，给TokenStream增加工具调用流式输出
    2.定义统一的流式消息格式

--- src/main/java/com/resky/yuaicodemother/core/AiCodeGeneratorFacade.java
+++ src/main/java/com/resky/yuaicodemother/core/AiCodeGeneratorFacade.java
@@ -1,14 +1,21 @@
 package com.resky.yuaicodemother.core;
 
+import cn.hutool.json.JSONUtil;
 import com.resky.yuaicodemother.ai.AiCodeGeneratorService;
 import com.resky.yuaicodemother.ai.AiCodeGeneratorServiceFactory;
 import com.resky.yuaicodemother.ai.model.HtmlCodeResult;
 import com.resky.yuaicodemother.ai.model.MultiFileCodeResult;
+import com.resky.yuaicodemother.ai.model.message.AiResponseMessage;
+import com.resky.yuaicodemother.ai.model.message.ToolExecutedMessage;
+import com.resky.yuaicodemother.ai.model.message.ToolRequestMessage;
 import com.resky.yuaicodemother.core.parser.CodeParserExecutor;
 import com.resky.yuaicodemother.core.saver.CodeFileSaverExecutor;
 import com.resky.yuaicodemother.exception.BusinessException;
 import com.resky.yuaicodemother.exception.ErrorCode;
 import com.resky.yuaicodemother.model.enums.CodeGenTypeEnum;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.service.TokenStream;
+import dev.langchain4j.service.tool.ToolExecution;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -80,11 +87,41 @@ public class AiCodeGeneratorFacade {
                 yield processCodeStream(codeStream, CodeGenTypeEnum.MULTI_FILE, appId);
             }
             case VUE_PROJECT -> {
-                Flux<String> codeStream = aiCodeGeneratorService.generateVueProjectCodeStream(appId,userMessage);
-                yield processCodeStream(codeStream, CodeGenTypeEnum.MULTI_FILE, appId);
+                TokenStream codeStream = aiCodeGeneratorService.generateVueProjectCodeStream(appId, userMessage);
+                yield processTokenStream(codeStream);
             }
             default -> throw new BusinessException(ErrorCode.SYSTEM_ERROR, "不支持的生成类型");
         };
+    }
+    /**
+     * 将 TokenStream 转换为 Flux<String>，并传递工具调用信息
+     *
+     * @param tokenStream TokenStream 对象
+     * @return Flux<String> 流式响应
+     */
+    private Flux<String> processTokenStream(TokenStream tokenStream) {
+        return Flux.create(sink -> {
+            tokenStream.onPartialResponse((String partialResponse) -> {
+                        AiResponseMessage aiResponseMessage = new AiResponseMessage(partialResponse);
+                        sink.next(JSONUtil.toJsonStr(aiResponseMessage));
+                    })
+                    .onPartialToolExecutionRequest((index, toolExecutionRequest) -> {
+                        ToolRequestMessage toolRequestMessage = new ToolRequestMessage(toolExecutionRequest);
+                        sink.next(JSONUtil.toJsonStr(toolRequestMessage));
+                    })
+                    .onToolExecuted((ToolExecution toolExecution) -> {
+                        ToolExecutedMessage toolExecutedMessage = new ToolExecutedMessage(toolExecution);
+                        sink.next(JSONUtil.toJsonStr(toolExecutedMessage));
+                    })
+                    .onCompleteResponse((ChatResponse response) -> {
+                        sink.complete();
+                    })
+                    .onError((Throwable error) -> {
+                        error.printStackTrace();
+                        sink.error(error);
+                    })
+                    .start();
+        });
     }
 
 
--- src/main/java/com/resky/yuaicodemother/service/impl/AppServiceImpl.java
+++ src/main/java/com/resky/yuaicodemother/service/impl/AppServiceImpl.java
@@ -10,6 +10,7 @@ import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
 import com.resky.yuaicodemother.constant.AppConstant;
 import com.resky.yuaicodemother.core.AiCodeGeneratorFacade;
+import com.resky.yuaicodemother.core.handler.StreamHandlerExecutor;
 import com.resky.yuaicodemother.exception.BusinessException;
 import com.resky.yuaicodemother.exception.ErrorCode;
 import com.resky.yuaicodemother.exception.ThrowUtils;
@@ -55,6 +56,9 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
     @Resource
     private ChatHistoryService chatHistoryService;
 
+    @Resource
+    private StreamHandlerExecutor streamHandlerExecutor;
+
     /**
      * 生成代码（流式）
      *
@@ -79,32 +83,13 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
         if (codeGenTypeEnum == null) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "不支持的生成类型");
         }
-        // 5.通过校验后，添加用户消息到对话历史
-        chatHistoryService.addChatMessage(appId, userMessage, "user", loginUser.getId());
-        // 6.调用AI生成代码（流式）
-        Flux<String> fluxContext = aiCodeGeneratorFacade.generateAndSaveCodeStream(userMessage, codeGenTypeEnum, appId);
-        // 7.收集AI响应内容并在完成后记录到对话历史
-        StringBuilder aiResponseBuilder = new StringBuilder();
-        return fluxContext
-                .map(chunk -> {
-                    // 收集AI 响应内容
-                    aiResponseBuilder.append(chunk);
-                    return chunk;
-                })
-                .doOnComplete(() -> {
-                    // 流式响应完成后，添加AI消息到对话历史
-                    String aiResponse = aiResponseBuilder.toString();
-                    if (StrUtil.isNotBlank(aiResponse)) {
-                        chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                    }
-                })
-                .doOnError(error -> {
-                    // 如果AI回复失败，记录错误信息
-                    String errorMessage = "AI回复失败：" + error.getMessage();
-                    if (StrUtil.isNotBlank(errorMessage)) {
-                        chatHistoryService.addChatMessage(appId, errorMessage, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                    }
-                });
+        // 5. 通过校验后，添加用户消息到对话历史
+        chatHistoryService.addChatMessage(appId, userMessage, ChatHistoryMessageTypeEnum.USER.getValue(), loginUser.getId());
+        // 6. 调用 AI 生成代码（流式）
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream(userMessage, codeGenTypeEnum, appId);
+        // 7. 收集 AI 响应内容并在完成后记录到对话历史
+        return streamHandlerExecutor.doExecute(codeStream, chatHistoryService, appId, loginUser, codeGenTypeEnum);
+
     }
 
     /**
