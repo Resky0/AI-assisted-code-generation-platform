2201af1b350ff26039a118a389cd719b67ea6592
Resky <1411011619@qq.com>
2026-04-23 20:06:15 +0800

    26.4.23 Vue项目部署功能
    1.针对Vue项目提供部署方法
    2.前端新增Vue项目枚举

--- src/main/java/com/resky/yuaicodemother/core/handler/JsonMessageStreamHandler.java
+++ src/main/java/com/resky/yuaicodemother/core/handler/JsonMessageStreamHandler.java
@@ -5,9 +5,12 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.resky.yuaicodemother.ai.model.message.*;
+import com.resky.yuaicodemother.constant.AppConstant;
+import com.resky.yuaicodemother.core.builder.VueProjectBuilder;
 import com.resky.yuaicodemother.model.entity.User;
 import com.resky.yuaicodemother.model.enums.ChatHistoryMessageTypeEnum;
 import com.resky.yuaicodemother.service.ChatHistoryService;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -22,6 +25,11 @@ import java.util.Set;
 @Slf4j
 @Component
 public class JsonMessageStreamHandler {
+    @Resource
+    private VueProjectBuilder vueProjectBuilder;
+
+
+
 
     /**
      * 处理 TokenStream（VUE_PROJECT）
@@ -50,6 +58,9 @@ public class JsonMessageStreamHandler {
                     // 流式响应完成后，添加 AI 消息到对话历史
                     String aiResponse = chatHistoryStringBuilder.toString();
                     chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
+                    // 异步构造 Vue 项目
+                    String projectPath = AppConstant.CODE_OUTPUT_ROOT_DIR + "/vue_project_" + appId;
+                    vueProjectBuilder.buildProjectAsync(projectPath);
                 })
                 .doOnError(error -> {
                     // 如果AI回复失败，也要记录错误消息
